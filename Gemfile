ce04c8a736b8047019197d17e302105ba1f2237e
Joshua Ling <joshling1919@gmail.com>
2019-06-17 08:19:45 -0700

    Set up factory_bot and rspec

--- Gemfile
+++ Gemfile
@@ -30,17 +30,16 @@ gem 'devise-jwt', '~> 0.5.9'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
-  gem 'rspec'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rspec-rails'
 end
 
 group :development do
   gem 'annotate'
   gem 'listen', '~> 3.0.5'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'pry-rails'
   gem 'rubocop-rails'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/factories/teachers.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: teachers
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  jti                    :string           not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+FactoryBot.define do
+  factory :teacher do
+    email { Faker::Internet.email }
+    password { 'abcdefg'}
+  end
+end
