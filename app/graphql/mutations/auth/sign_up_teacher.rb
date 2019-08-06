1891cae96786ef1908dc56ecd43cf6a83c769930
Joshua Ling <joshling1919@gmail.com>
2019-08-06 09:50:50 -0700

    Add teacher type

---
+++ app/graphql/mutations/auth/sign_up_teacher.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Auth
+    class SignUpTeacher < BaseMutation
+      graphql_name 'Sign Up Teacher'
+      description 'Signs teacher up for an account'
+
+      # arguments passed to the `resolved` method
+      argument :email, String, required: true
+      argument :password, String, required: true
+
+      # return type from the mutation
+      type Types::TeacherType
+
+      def resolve(email:, password:)
+        Teacher.create(
+          email: email,
+          password: password
+        )
+      end
+
+      # sample mutation
+      # mutation {
+      #   signUpTeacher(email: "test@example.com", password: "password1243") {
+      #     email
+      #     id
+      #   }
+      # }
+    end
+  end
+end
