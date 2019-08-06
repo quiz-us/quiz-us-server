aa0fced3e76ce5f85e9eaf548ee411b35632dfba
Joshua Ling <joshling1919@gmail.com>
2019-08-06 16:25:05 -0700

    Lint routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :teachers
-  
+
   # for debugging
   # access graphiql with GET http://localhost:3000/graphiql
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
     # at: is the path where GraphiQL will be served. You can access GraphiQL by visiting that path in your app.
     # graphql_path: is the path to the GraphQL endpoint. GraphiQL will send queries to this path.
   end
@@ -13,7 +15,6 @@ Rails.application.routes.draw do
     resources :questions, only: [:create]
   end
 
-  #react components will use this endpt
-  post "/graphql", to: "graphql#execute"
+  # react components will use this endpt
+  post '/graphql', to: 'graphql#execute'
 end
-
