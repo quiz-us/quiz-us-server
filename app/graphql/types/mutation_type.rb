ddfe0a218b30616bd330250a72302602329eabcb
Joshua Ling <joshling1919@gmail.com>
2019-08-06 20:37:27 -0700

    Add sign_up_teacher mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Types
   class MutationType < BaseObject
     field :edit_standards_chart, mutation: Mutations::EditStandardsChart
+    field :sign_up_teacher, mutation: Mutations::Auth::SignUpTeacher
   end
 end
