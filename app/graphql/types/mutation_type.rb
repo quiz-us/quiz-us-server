aafa2250ef4703b1ed939f3188064747d280993c
Joshua Ling <joshling1919@gmail.com>
2019-08-11 16:43:37 -0700

    Add log_in_teacher to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < BaseObject
     field :edit_standards_chart, mutation: Mutations::EditStandardsChart
     field :sign_up_teacher, mutation: Mutations::Auth::SignUpTeacher
+    field :log_in_teacher, mutation: Mutations::Auth::LogInTeacher
   end
 end
