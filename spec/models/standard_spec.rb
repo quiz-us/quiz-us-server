a49ef960403abcfae9289261da217f70f6dd14f2
Joshua Ling <joshling1919@gmail.com>
2019-08-14 08:09:10 -0700

    Finish standards model spec

--- spec/models/standard_spec.rb
+++ spec/models/standard_spec.rb
@@ -16,6 +16,7 @@
 require 'rails_helper'
 
 RSpec.describe Standard, type: :model do
+  subject { create(:standard) }
   describe 'associations' do
     it { should belong_to(:standards_category) }
     it { should have_many(:questions_standards) }
@@ -23,10 +24,16 @@ RSpec.describe Standard, type: :model do
   end
 
   describe 'validations' do
-    subject { create(:standard) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:standards_category) }
     it { should validate_presence_of(:title) }
     it { should validate_uniqueness_of(:title).scoped_to(:standards_category_id) }
+  end
+
+  describe '#standards_chart' do
+    it "should delegate #standards_chart to standards_category's association" do
+      category = subject.standards_category
+      expect(subject.standards_chart).to be(category.standards_chart)
+    end
   end
 end
