146e205fa804cfc384c59b32f346ffc7be5488e5
Joshua Ling <joshling1919@gmail.com>
2019-08-14 07:32:45 -0700

    Move cors logic to cors file

--- config/application.rb
+++ config/application.rb
@@ -29,17 +29,6 @@ module QuizUsServer
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: %i[get patch put delete post options]
-        )
-      end
-    end
-
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.view_specs      false
