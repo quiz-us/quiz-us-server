6b7825c4a54450dd4f146de853c29d4a2695fc3a
Joshua Ling <joshling1919@gmail.com>
2019-08-16 15:34:06 -0700

    Nest standards under course in query

--- app/graphql/queries/standard_index.rb
+++ app/graphql/queries/standard_index.rb
@@ -4,11 +4,17 @@ module Queries
   class StandardIndex < BaseQuery
     description 'Display all standards'
 
+    # argument :course_id, ID, required: true
+
     type [Types::StandardType], null: false
 
     def resolve
       teacher_signed_in?
-      current_teacher.standards
+      # @todo: in the future, if a teacher has more than one course, we would
+      # need to add a course switching interface in the frontend and pass
+      # up current_course with each request. For example:
+      # current_teacher.courses.find(:id).standards
+      current_teacher.courses.first.standards
     end
   end
 end
