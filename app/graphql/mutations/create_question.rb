b778fc753d2156b5cc3ea704e5a509c94c4530b0
Cynthia Ma <cma@zendesk.com>
2019-08-17 19:12:34 -0700

    Add working solution for adding question options as type string

--- app/graphql/mutations/create_question.rb
+++ app/graphql/mutations/create_question.rb
@@ -10,33 +10,25 @@ module Mutations
     argument :tags, [String], required: false
     argument :question_node, String, required: true
     argument :question_plaintext, String, required: true
-    # argument :answers, [Types::QuestionOptionType], required: false
-    # argument :tags_test, [Types::TagType], required: false
-    field :answers, [Types::QuestionOptionType], null: false do
-      argument :attributes, Types::QuestionOptionType, required: true
-    end
+    argument :question_options, [String], required: false
 
     # return type from the mutation
     type Types::QuestionType
 
-    def resolve(question_node: , question_type:, tags:, standard_id:, question_plaintext: )
-      question = CreateQuestionService.call({ 
-        question: {
-          question_node: question_node,
-          question_type: question_type,
-          tags: tags,
-          standard_id: standard_id,
-          question_plaintext: question_plaintext
-          # answers: answers
-        }
+    def resolve(question_node: , question_type:, tags:, standard_id:, question_plaintext:, question_options: )
+      CreateQuestionService.call({ 
+        question_node: question_node,
+        question_type: question_type,
+        tags: tags,
+        standard_id: standard_id,
+        question_plaintext: question_plaintext,
+        question_options: question_options
       })
-      question[:question]
     end
   end
 end
 
 # sample mutation
-
 # mutation {
 #   createQuestion(questionNode: """
 #     {      
@@ -51,9 +43,22 @@ end
 # 		}
     
     
-#     """, 
-#   	questionType: "mc",
-#   	questionTags: "sci, art"
+#     """
+#   	questionType: "mc"
+#   	tags: ["sci", "art"]
+#   	questionPlaintext: "asdf"
+#   	standardId: 1,
+#   	questionOptions: ["""{
+#   		"correct": "true",
+#   		"optionNode": "{}",
+#   		"optionText": "correct answer"
+# }""", 
+#   """{
+#   		"correct": "false",
+#   		"optionNode": "{}",
+#   		"optionText": "not correct answer"
+# }"""
+# ]
 # ) {
 #     id
 #   	tags {
@@ -63,6 +68,3 @@ end
 #   questionText
 #   } 
 # }
-
-
-
