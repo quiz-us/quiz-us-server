3b3c11e6cfe900f2c16acd539a75a08746704d5b
Joshua Ling <joshling1919@gmail.com>
2019-08-21 15:12:19 -0700

    Add uniqueness constraint to tags name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_221415) do
+ActiveRecord::Schema.define(version: 2019_08_21_221111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,6 +157,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_221415) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "teachers", id: :serial, force: :cascade do |t|
