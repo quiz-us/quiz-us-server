2092b9bb0268554f314d7980682dc91d0dc93925
Joshua Ling <joshling1919@gmail.com>
2019-08-26 08:54:16 -0700

    Add due to assignment type

--- app/graphql/types/assignment_type.rb
+++ app/graphql/types/assignment_type.rb
@@ -6,6 +6,7 @@ module Types
     field :id, ID, null: false
     field :instructions, String, null: true
     field :deck, Types::DeckType, null: false
+    field :due, Types::DateTimeType, null: true
     field :period, Types::PeriodType, null: false
     field :responses, [Types::ResponseType], null: true
   end
