7bd81f7e3b74a266e2cdf4c187b977fd739f9c72
Joshua Ling <joshling1919@gmail.com>
2019-08-27 17:53:29 -0700

    Fix migration

--- db/migrate/20190827235134_change_node_types_to_json.rb
+++ db/migrate/20190827235134_change_node_types_to_json.rb
@@ -2,10 +2,10 @@
 
 class ChangeNodeTypesToJson < ActiveRecord::Migration[5.2]
   def change
-    remove_column :questions, :rich_text, :string
+    remove_column :questions, :question_node, :string
     add_column :questions, :rich_text, :jsonb, default: {}
 
-    remove_column :question_options, :rich_text, :string
+    remove_column :question_options, :option_node, :string
     add_column :question_options, :rich_text, :jsonb, default: {}
   end
 end
