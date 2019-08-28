3eea45ade665b9541e151327621439a5fb662b09
Joshua Ling <joshling1919@gmail.com>
2019-08-28 09:26:00 -0700

    Refactor create_response

--- app/graphql/mutations/students/create_response.rb
+++ app/graphql/mutations/students/create_response.rb
@@ -27,15 +27,16 @@ module Mutations
         personal_deck = current_student.personal_decks.first
         card = personal_deck.cards.find_by(question_id: question_id) ||
                personal_deck.cards.create!(question_id: question_id)
-        if question_type == 'Multiple Choice'
-          # determine when they need to see the question again
-          # either add it to personal deck with newly calculated date
-          # or find existing deck_questions and set the next review date to
-          # newly calculated date
-        elsif question_type == 'Free Response'
-          # handle accordingly
-          CalculateDue.new(self_grade.to_i, card).call
+
+        case question_type
+        when 'Multiple Choice'
+          rating = QuestionOption.find(question_option_id).correct ? 4 : 1
+        when 'Free Response'
+          rating = self_grade.to_i
+        else
+          raise StandardError('That Question Type is not currently supported!')
         end
+        CalculateDue.new(rating, card).call
         response
       end
     end
