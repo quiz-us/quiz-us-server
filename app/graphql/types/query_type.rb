12f7044627fad3343b7f895679e7117b835ae2ea
Joshua Ling <joshling1919@gmail.com>
2019-09-04 07:38:42 -0700

    Add period_assignment_index

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,6 +8,7 @@ module Types
     field :deck, resolver: Queries::DeckShow
     field :decks, resolver: Queries::DeckIndex
     field :period, resolver: Queries::PeriodShow
+    field :period_assignments, resolver: Queries::PeriodAssignmentIndex
     field :periods, resolver: Queries::PeriodIndex
     field :standards_charts, resolver: Queries::StandardsChartIndex
     field :question, resolver: Queries::QuestionShow
