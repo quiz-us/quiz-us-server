c2035242b1c96b52aae8bdfb848fa25c725ad13d
Cynthia Ma <cma@zendesk.com>
2019-09-13 08:20:34 -0700

    Initial commit

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,23 +3,28 @@
 module Types
   class MutationType < BaseObject
     # TEACHER MUTATIONS:
-    field :create_assignments, mutation: Mutations::CreateAssignments
-    field :create_deck, mutation: Mutations::CreateDeck
-    field :create_login_link, mutation: Mutations::CreateLoginLink
-    field :create_period, mutation: Mutations::CreatePeriod
-    field :create_question, mutation: Mutations::CreateQuestion
-    field :delete_deck, mutation: Mutations::DeleteDeck
-    field :edit_standards_chart, mutation: Mutations::EditStandardsChart
     field :enroll_student, mutation: Mutations::EnrollStudent
     field :log_in_teacher, mutation: Mutations::Auth::LogInTeacher
     field :sign_up_teacher, mutation: Mutations::Auth::SignUpTeacher
+    field :create_login_link, mutation: Mutations::CreateLoginLink
+
+    field :create_period, mutation: Mutations::CreatePeriod
+    field :create_assignments, mutation: Mutations::CreateAssignments
+    field :edit_standards_chart, mutation: Mutations::EditStandardsChart
+
+    field :create_deck, mutation: Mutations::CreateDeck
     field :update_deck, mutation: Mutations::UpdateDeck
+    field :delete_deck, mutation: Mutations::DeleteDeck
+
+    field :create_question, mutation: Mutations::CreateQuestion
+    field :update_question, mutation: Mutations::UpdateQuestion
     field :delete_question, mutation: Mutations::DeleteQuestion
 
     # STUDENT MUTATIONS:
-    field :create_response, mutation: Mutations::Students::CreateResponse
     field :log_in_student, mutation: Mutations::Auth::LogInStudent
     field :log_out_student, mutation: Mutations::Auth::LogOutStudent
     field :qr_log_in_student, mutation: Mutations::Auth::QrLogInStudent
+
+    field :create_response, mutation: Mutations::Students::CreateResponse
   end
 end
