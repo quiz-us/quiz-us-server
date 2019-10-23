070d6d467e579ab20af1e4c6fbb6f560df86ace0
Joshua Ling <joshling1919@gmail.com>
2019-10-23 08:42:37 -0700

    Add unenroll student

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,13 +9,14 @@ module Types
     field :create_question, mutation: Mutations::Teachers::CreateQuestion
     field :delete_deck, mutation: Mutations::Teachers::DeleteDeck
     field :delete_period, mutation: Mutations::Teachers::DeletePeriod
+    field :delete_question, mutation: Mutations::Teachers::DeleteQuestion
     field :edit_period, mutation: Mutations::Teachers::EditPeriod
     field :edit_standards_chart, mutation: Mutations::Teachers::EditStandardsChart
     field :enroll_student, mutation: Mutations::Teachers::EnrollStudent
     field :log_in_teacher, mutation: Mutations::Teachers::LogInTeacher
     field :sign_up_teacher, mutation: Mutations::Teachers::SignUpTeacher
+    field :unenroll_student, mutation: Mutations::Teachers::UnenrollStudent
     field :update_deck, mutation: Mutations::Teachers::UpdateDeck
-    field :delete_question, mutation: Mutations::Teachers::DeleteQuestion
 
     # # STUDENT MUTATIONS:
     field :create_login_link, mutation: Mutations::Students::CreateLoginLink
---
+++ spec/graphql/mutations/teachers/unenroll_student_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Mutations::Teachers::UnenrollStudent' do
+  let(:student) { create(:student) }
+  let(:period) { create(:period) }
+  let(:query_string) do
+    <<-GRAPHQL
+      mutation unenrollStudent($studentId: ID!, $periodId: ID!) {
+        unenrollStudent(studentId: $studentId, periodId: $periodId) {
+          id
+          firstName
+          lastName
+        }
+      }
+    GRAPHQL
+  end
+  let(:variables) do
+    {
+      studentId: student.id,
+      periodId: period.id
+    }
+  end
+
+  context 'when teacher is not signed in' do
+    it 'returns Unauthenticated error' do
+      errors = QuizUsServerSchema.execute(query_string, variables: variables)
+                                 .to_h['errors']
+      expect(errors[0]['message']).to eq('Unauthenticated')
+    end
+  end
+
+  context 'when teacher is signed in' do
+    let(:teacher) { create(:teacher) }
+    before(:each) do
+      # stub out context[:current_teacher] to get around authentication:
+      allow_any_instance_of(Mutations::BaseMutation).to receive(:current_teacher)
+        .and_return(teacher)
+    end
+
+    context 'when period does not belong to a teacher' do
+      it 'raises a GraphQL execution error' do
+        errors = QuizUsServerSchema.execute(query_string, variables: variables)
+                                   .to_h['errors']
+
+        expect(errors[0]['message']).to eq(
+          'You can only unenroll students in your classes'
+        )
+      end
+    end
+
+    context 'when a period does belong to a teacher' do
+      let(:valid_course) { create(:course, teacher: teacher) }
+      let(:valid_period) { create(:period, course: valid_course) }
+      before(:each) do
+        create(:enrollment, student: student, period: valid_period)
+      end
+      it 'unenrolls the student from the period by destroying the enrollment' do
+        results = QuizUsServerSchema.execute(query_string, variables: {
+                                               studentId: student.id,
+                                               periodId: valid_period.id
+                                             }).to_h['data']['unenrollStudent']
+
+        expect(results['firstName']).to eq(student.first_name)
+        expect(results['lastName']).to eq(student.last_name)
+        expect(results['id'].to_i).to eq(student.id)
+      end
+    end
+  end
+end
