79ba35649ecc6bfbb9348ea5a3fc4fbd8397510d
Joshua Ling <joshling1919@gmail.com>
2019-11-18 18:29:11 -0800

    Alphabetize query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,29 +5,30 @@
 module Types
   class QueryType < BaseObject
     # TEACHER TYPES
+    field :all_standards, resolver: Queries::Teachers::StandardIndex
     field :assignment_results, resolver: Queries::Teachers::AssignmentResults
     field :deck, resolver: Queries::Teachers::DeckShow
     field :decks, resolver: Queries::Teachers::DeckIndex
-    field :period, resolver: Queries::Teachers::PeriodShow
     field :period_assignments, resolver: Queries::Teachers::PeriodAssignmentIndex
     field :period_standards_mastery, resolver: Queries::Teachers::PeriodStandardsMastery
+    field :period, resolver: Queries::Teachers::PeriodShow
     field :periods, resolver: Queries::Teachers::PeriodIndex
+    field :question_option, resolver: Queries::Teachers::QuestionOptionShow
     field :question, resolver: Queries::Teachers::QuestionShow
     field :questions, resolver: Queries::Teachers::QuestionSearch
+    field :standard, resolver: Queries::Teachers::StandardShow
     field :standards_charts, resolver: Queries::Teachers::StandardsChartIndex
+    field :student_assignment_results, resolver: Queries::Teachers::StudentAssignmentResults
+    field :students, resolver: Queries::Teachers::StudentIndex
+    field :tag_search, resolver: Queries::Teachers::TagSearch
     field :tag, resolver: Queries::Teachers::TagShow
     field :tagging, resolver: Queries::Teachers::TaggingShow
-    field :standard, resolver: Queries::Teachers::StandardShow
-    field :all_standards, resolver: Queries::Teachers::StandardIndex
-    field :question_option, resolver: Queries::Teachers::QuestionOptionShow
-    field :students, resolver: Queries::Teachers::StudentIndex
     field :tags, resolver: Queries::Teachers::TagIndex
-    field :tag_search, resolver: Queries::Teachers::TagSearch
     field :teacher_assignment, resolver: Queries::Teachers::AssignmentShow
 
     # STUDENT TYPES
-    field :current_student, resolver: Queries::Students::CurrentStudent
     field :assignment, resolver: Queries::Students::AssignmentShow
+    field :current_student, resolver: Queries::Students::CurrentStudent
     field :personal_assignment, resolver: Queries::Students::PersonalAssignment
     field :student_assignments, resolver: Queries::Students::AssignmentIndex
   end
