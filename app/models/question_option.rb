71cfd8ca6b7c4af631a091c8ef6aa6c7986ac090
Joshua Ling <joshling1919@gmail.com>
2020-01-16 07:28:28 -0800

    Add documentation for optional question option

--- app/models/question_option.rb
+++ app/models/question_option.rb
@@ -14,5 +14,9 @@
 #
 
 class QuestionOption < ApplicationRecord
-  belongs_to :question
+  # We're allowing question options to be orphaned for analytics purposes.
+  # For example, we want a record of answer choices that students have
+  # submitted in the past, even if the teacher eventually deleted that answer
+  # choice from a question:
+  belongs_to :question, optional: true
 end
