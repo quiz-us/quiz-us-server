78e40d063982e509952a1dc77604a9e3d3397780
Joshua Ling <joshling1919@gmail.com>
2020-02-22 14:48:35 -0800

    Bump token expiration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,7 +305,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 1.day.to_i
+    jwt.expiration_time = 7.days.to_i
   end
 
   config.navigational_formats = []
